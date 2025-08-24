10c4a7c20d3cc2e458a69d004acf91a441f6e4cb
Renzo Lo Presti <89809425+renzlopgit@users.noreply.github.com>
2025-08-24 21:37:37 +0200

    Delete assets directory

--- assets/js/main.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("language-toggle").addEventListener("change", function() {
-    if (this.checked) {
-        window.location.href = "/en";
-    } else {
-        window.location.href = "/it";
-    }
-});
