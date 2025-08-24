73a91a5d04371151c267e2f76adb52958db65055
Renzo Lo Presti <89809425+renzlopgit@users.noreply.github.com>
2025-08-24 23:25:47 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,9 @@ document.getElementById("language-toggle").addEventListener("change", function()
     // Verifica se il checkbox è selezionato (EN)
     if (this.checked) {
         // Se il checkbox è selezionato, EN
-        window.location.href = "/en";
+        window.location.href = "/siteWIP/en";
     } else {
         // Se il checkbox non è selezionato, IT
-        window.location.href = "/it";
+        window.location.href = "/siteWIP/it";
     }
 });
